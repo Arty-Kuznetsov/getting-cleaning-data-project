79f6c093616260933fe846135b47a73115139012
Thomas Swann <ts.swann@gmail.com>
2015-03-17 11:45:15 +0000

    Remove download code. Assume data will be present as pre-requisite

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,11 @@
+library(dplyr)
+library(tidyr)
 
+# This function assumes UCI HAR Dataset sub-directory containing all Samsung instrumentation data
+# exists within the same directory as this source file.
+# Download from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 run_analysis <- function() {
-  data.base.dir <- "./data"
-  data.dir <- paste(data.base.dir, "UCI HAR Dataset", sep="")
-  get.dataset(data.base.dir)
+  setwd('./UCI HAR Dataset')
   
   
-}
-
-# Retrieve and extract raw Samsung Smartphone data for cleansing
-get.dataset <- function(base.dir) {
-  zipfile <- paste(base.dir, "/dataset.zip", sep="")
-  # Create data directory for downloaded files
-  if (!file.exists(base.dir)) {
-    dir.create(base.dir)
-  }
-  
-  if (!file.exists(zipfile)) {
-    download.file('https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip', 
-                  method='curl', destfile = zipfile)
-  }
-  
-  unzip(zipfile, overwrite = TRUE, exdir = base.dir)
 }
